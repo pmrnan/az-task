e2ebc15fe50a530d45fe2466962aebd8aa08a7f1
pmrnan <marubee76@gmail.com>
2025-06-12 01:13:58 +0900

    [fix]TaskItemの並びに合わせて優先度と期日のフォーム位置を修正

--- src/components/top/AddTaskForm/index.tsx
+++ src/components/top/AddTaskForm/index.tsx
@@ -18,8 +18,13 @@ export const AddTaskForm = () => {
         <div className="my-4 mx-16 grid grid-cols-[15fr_1fr] items-center ">
           <div className="grid grid-cols-[2fr_1fr_1fr] gap-4">
             <AppTextForm name="task_name" title="タスク名" form={form} />
+            <AppSelectForm
+              name="priority"
+              title="優先度"
+              form={form}
+              options={PRIORITY_OPTION}
+            />
             <AppDateForm name="limit_date" title="期日" form={form} />
-            <AppSelectForm name="priority" title="優先度" form={form} options={PRIORITY_OPTION} />
           </div>
           <div className="ml-auto">
             <Button type="submit" className="submit-button">

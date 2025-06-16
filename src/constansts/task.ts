4381f46510e4f27547b5ea97b4d0c56f3ce525af
pmrnan <marubee76@gmail.com>
2025-06-16 16:52:22 +0900

    [refactor]ステータス・優先度の日本語名を定数化

--- src/constansts/task.ts
+++ src/constansts/task.ts
@@ -6,21 +6,35 @@ export const STATUS_NOT_STARTED = 'NOT_STARTED';
 export const STATUS_DOING = 'DOING';
 export const STATUS_DONE = 'DONE';
 
+// ステータス名称取得
+export const getStatusName = (status: string) => {
+    switch (status) {
+        case STATUS_NOT_STARTED:
+            return '未着手';
+        case STATUS_DOING:
+            return '着手中';
+        case STATUS_DONE:
+            return '完了';
+        default:
+            return '';
+    }
+}
+
 // タスクフィールドのステータス
 export const FIELDS_STATUS: FieldsStatus = {
     notStarted: {
         key: STATUS_NOT_STARTED,
-        statusName: '未着手',
+        statusName: getStatusName(STATUS_NOT_STARTED),
         badgeClass: 'task-status-badge-not-started'
     },
     doing: {
         key: STATUS_DOING,
-        statusName: '着手中',
+        statusName: getStatusName(STATUS_DOING),
         badgeClass: 'task-status-badge-doing'
     },
     done: {
         key: STATUS_DONE,
-        statusName: '完了',
+        statusName: getStatusName(STATUS_DONE),
         badgeClass: 'task-status-badge-done'
     },
 }
@@ -30,19 +44,33 @@ export const PRIORITY_HIGH = 'HIGH';
 export const PRIORITY_MIDDLE = 'MIDDLE';
 export const PRIORITY_LOW = 'LOW';
 
+// 優先度名称取得
+export const getPriorityName = (priority: string) => {
+    switch (priority) {
+        case PRIORITY_HIGH:
+            return '高';
+        case PRIORITY_MIDDLE:
+            return '中';
+        case PRIORITY_LOW:
+            return '低';
+        default:
+            return '';
+    }
+}
+
 // 優先度選択肢
 export const PRIORITY_OPTION: Option[] = [
     {
         value: PRIORITY_HIGH,
-        label: '高'
+        label: getPriorityName(PRIORITY_HIGH),
     },
     {
         value: PRIORITY_MIDDLE,
-        label: '中'
+        label: getPriorityName(PRIORITY_MIDDLE),
     },
     {
         value: PRIORITY_LOW,
-        label: '低'
+        label: getPriorityName(PRIORITY_LOW),
     },
 ]
 
